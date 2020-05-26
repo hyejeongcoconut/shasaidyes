06daeed5a50a845984c4cd39bd892f9221f50d76
Michelle Kim <mkkeems@gmail.com>
2020-05-27 01:08:53 +0900

    landing page and navbar modifications

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_05_26_072419) do
-
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -121,9 +118,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_072419) do
   add_foreign_key "favorite_vendors", "users"
   add_foreign_key "favorite_vendors", "vendors"
   add_foreign_key "guests", "users"
-
   add_foreign_key "product_services", "vendors"
-
   add_foreign_key "quotes", "users"
   add_foreign_key "quotes", "vendors"
 end
