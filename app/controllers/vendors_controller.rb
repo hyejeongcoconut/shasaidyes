212e0730c5649aef75ca489c425156a2633d8a79
Hyejeong Kwon <hyejeong206@gmail.com>
2020-05-27 17:35:30 +0900

    Edited vendor controller and product controller

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -6,42 +6,9 @@ class VendorsController < ApplicationController
     @vendors = Vendor.all
   end
 
-  def new
-    @vendor = Vendor.new
-    authorize @vender
-  end
-
   def show
     @vendor = Vendor.find(params[:id])
     authorize @vender
   end
 
-  def create
-    @vendor = Vendor.new(vendor_params)
-    @vendor.save
-    authorize @vender
-  end
-
-  def edit
-    @vendor = Vendor.find(params[:id])
-    authorize @vendor
-  end
-
-  def update
-    @vendor = Vendor.find(params[:id])
-    @vendor.update(vendor_params)
-    authorize @vendor
-  end
-
-  def destroy
-    @vendor = Vendor.find(params[:id])
-    @vendor.destroy
-    authorize @vender
-  end
-
-  private
-
-  def vendor_params
-    params.require(:vendor).permit(:name, :category, :phone_number, :description, :email)
-  end
 end
