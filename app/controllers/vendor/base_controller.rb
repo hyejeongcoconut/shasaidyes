41fc5024d02303772c6542c5056715d087c10bd2
Michelle Kim <mkkeems@gmail.com>
2020-05-28 17:40:07 +0900

    vendors search index page donw for now

--- app/controllers/vendor/base_controller.rb
+++ app/controllers/vendor/base_controller.rb
@@ -1,5 +1,16 @@
 class Vendor::BaseController < ApplicationController
-    before_action :authenticate_vendor!
+  before_action :authenticate_vendor!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :photo, :category, :phone_number, :description, :address, :city])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :photo, :category, :phone_number, :description, :address, :city])
+  end
+
+
 
     private
   
