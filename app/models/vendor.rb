9ff2ca94434e243283cf92e5516a88f74986f6ef
YiroYi <yiroyi@live.com>
2020-05-31 00:04:25 +0900

    Search with AJAX & Images Implemented

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,4 +1,5 @@
 class Vendor < ApplicationRecord
+  include Rails.application.routes.url_helpers
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   category = ["Reception Venue", "Photographers", "Bridal Salons", "Wedding Planner", "Wedding Cakes", "Honeymoon", "Caterers"]
@@ -12,9 +13,18 @@ class Vendor < ApplicationRecord
 
   has_one_attached :photo
 
+  def photo_url
+    rails_blob_path(self.photo, disposition: "attachment", only_path: true)
+  end
+
+  def hello
+    puts 'Hello'
+  end
+
   include PgSearch::Model
   pg_search_scope :search_by_category_and_price_and_city,
-    against: [ :category, :city ],
+    against: [ :category,
+               :city ],
   using: {
     tsearch: { prefix: true } # <-- now `superman batm` will return something!
   }
