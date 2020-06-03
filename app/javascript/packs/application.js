2cc9e3868eac879c92eccb77864f4b5cd3d9c9eb
Michelle Kim <mkkeems@gmail.com>
2020-06-03 10:35:53 +0900

    removed owl carousel, readded jquery lol no more error messages on console

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-window.jQuery = $;
-window.$ = window.jQuery = require('packs/jquery')
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -29,7 +28,6 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 
 import { initInboxCable } from '../channels/inbox_channel.js'
-// global.$ = require('jquery')
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
@@ -40,6 +38,3 @@ document.addEventListener('turbolinks:load', () => {
 });
 
 import "controllers"
-
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel';
