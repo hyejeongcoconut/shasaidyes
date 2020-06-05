a2e04f8f87d5925c34977eeecaff264cc208abdd
Michelle Kim <mkkeems@gmail.com>
2020-06-05 11:27:24 +0900

    some css changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,60 +12,60 @@ Guest.destroy_all
 Vendor.destroy_all
 Product.destroy_all
 
-h = User.new(
-  first_name: "Hyejeong",
-  last_name: "Kwon",
-  email: "hyejeongcoconut@test.com",
-  password:"123456"
-)
-puts "You has unlocked a new character Hyejeong created!"
+# h = User.new(
+#   first_name: "Hyejeong",
+#   last_name: "Kwon",
+#   email: "hyejeongcoconut@test.com",
+#   password:"123456"
+# )
+# puts "You has unlocked a new character Hyejeong created!"
 
-h.photo.attach(io: URI.open('https://d29zunrt9sid73.cloudfront.net/speaker_media/asset/28695/portrait_70_28695.png'), filename: 'pokemon.png', content_type: 'image/png')
-h.save!
+# h.photo.attach(io: File.open('app/assets/images/YmdI9lLq_400x400.jpg'), filename: 'pokemon.png', content_type: 'image/jpg')
+# h.save!
 
-m = User.new(
-  first_name: "Michelle",
-  last_name: "Kim",
-  email: "michelle@test.com",
-  password:"123456"
-)
-puts "You has unlocked a new character Michelle done!"
+# m = User.new(
+#   first_name: "Michelle",
+#   last_name: "Kim",
+#   email: "michelle@test.com",
+#   password:"123456"
+# )
+# puts "You has unlocked a new character Michelle done!"
 
-m.photo.attach(io: URI.open('https://d29zunrt9sid73.cloudfront.net/speaker_media/asset/28695/portrait_70_28695.png'), filename: 'pokemon.png', content_type: 'image/png')
-m.save!
+# m.photo.attach(io: URI.open('https://d29zunrt9sid73.cloudfront.net/speaker_media/asset/28695/portrait_70_28695.png'), filename: 'pokemon.png', content_type: 'image/png')
+# m.save!
 
-y = User.new(
-  first_name: "Yiro",
-  last_name: "Yi",
-  email: "yiroyi@test.com",
-  password:"123456"
-)
-puts "You has unlocked a new character Yiro done!"
+# y = User.new(
+#   first_name: "Yiro",
+#   last_name: "Yi",
+#   email: "yiroyi@test.com",
+#   password:"123456"
+# )
+# puts "You has unlocked a new character Yiro done!"
 
-y.photo.attach(io: URI.open('https://d29zunrt9sid73.cloudfront.net/speaker_media/asset/28695/portrait_70_28695.png'), filename: 'pokemon.png', content_type: 'image/png')
-y.save!
+# y.photo.attach(io: URI.open('https://d29zunrt9sid73.cloudfront.net/speaker_media/asset/28695/portrait_70_28695.png'), filename: 'pokemon.png', content_type: 'image/png')
+# y.save!
 
-preferences = ["Kimchi","Tostadas","Burguers","Malaxiangguo"]
 
-all = User.all
-ids = []
 
-all.each {|x| ids << x.id }
+# all = User.all
+# ids = []
 
-20.times do
-  Guest.create(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    phone_number:Faker::PhoneNumber.cell_phone,
-    address: Faker::Address.street_address,
-    email: Faker::Internet.email,
-    rsvp: 0,
-    additional_guest_numbers: rand(1..5),
-    food_preference: preferences.sample,
-    user_id: ids.sample
-  )
-  puts "Done !"
-end
+# all.each {|x| ids << x.id }
+
+# 20.times do
+#   Guest.create(
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     phone_number:Faker::PhoneNumber.cell_phone,
+#     address: Faker::Address.street_address,
+#     email: Faker::Internet.email,
+#     rsvp: 0,
+#     additional_guest_numbers: rand(1..5),
+#     food_preference: preferences.sample,
+#     user_id: ids.sample
+#   )
+#   puts "Done !"
+# end
 #------vendor seed ----------------------------------------------------------------------------------------
 category = Vendor::CATEGORY
 lovingyou = Vendor.new(
